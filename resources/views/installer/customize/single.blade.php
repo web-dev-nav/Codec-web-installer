26bca3d44c888e96b41f5ac3f42151a7741e24ed
Codelone <web.dev.nav@gmail.com>
2025-08-29 01:17:21 -0400

    php dropdown customize fix

--- resources/views/installer/customize/single.blade.php
+++ resources/views/installer/customize/single.blade.php
@@ -54,12 +54,13 @@
                 
                 <label>Minimum PHP Version:</label>
                 <select name="php_version">
-                    <option value="7.4.0" {{ ($currentConfig['requirements']['php'] ?? '') == '7.4.0' ? 'selected' : '' }}>PHP 7.4.0</option>
-                    <option value="8.0.0" {{ ($currentConfig['requirements']['php'] ?? '') == '8.0.0' ? 'selected' : '' }}>PHP 8.0.0</option>
-                    <option value="8.1.0" {{ ($currentConfig['requirements']['php'] ?? '') == '8.1.0' ? 'selected' : '' }}>PHP 8.1.0</option>
-                    <option value="8.2.0" {{ ($currentConfig['requirements']['php'] ?? '') == '8.2.0' ? 'selected' : '' }}>PHP 8.2.0</option>
-                    <option value="8.3.0" {{ ($currentConfig['requirements']['php'] ?? '') == '8.3.0' ? 'selected' : '' }}>PHP 8.3.0</option>
-                    <option value="8.4.0" {{ ($currentConfig['requirements']['php'] ?? '') == '8.4.0' ? 'selected' : '' }}>PHP 8.4.0</option>
+                    @php $currentPhp = $currentConfig['requirements']['php'] ?? '8.2.0'; @endphp
+                    <option value="7.4.0" {{ $currentPhp == '7.4.0' ? 'selected' : '' }}>PHP 7.4.0</option>
+                    <option value="8.0.0" {{ $currentPhp == '8.0.0' ? 'selected' : '' }}>PHP 8.0.0</option>
+                    <option value="8.1.0" {{ $currentPhp == '8.1.0' ? 'selected' : '' }}>PHP 8.1.0</option>
+                    <option value="8.2.0" {{ $currentPhp == '8.2.0' ? 'selected' : '' }}>PHP 8.2.0</option>
+                    <option value="8.3.0" {{ $currentPhp == '8.3.0' ? 'selected' : '' }}>PHP 8.3.0</option>
+                    <option value="8.4.0" {{ $currentPhp == '8.4.0' ? 'selected' : '' }}>PHP 8.4.0</option>
                 </select>
 
                 <label>Required PHP Extensions:</label>
